42e16a2603aa0a46fea8bb3dffe848770774254e
Capellan11 <aureocapellan11@gmail.com>
2024-10-30 21:25:56 +0800

    Create classSubject.js

---
+++ classSubject.js
@@ -0,0 +1,7 @@
+let subjectTitle = "Data Structures & Algorithms";
+let classSchedule = "Monday, Tuesday, Thursday, Friday 10:00 AM - 11:00 AM, 8:00 AM - 9:30 AM ";
+let classroom = "Comlab B";
+let classInstructor = "Clifford Owen Pascua";
+let studentName = "Aureo Leaño Capellan jr.";
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}`);
